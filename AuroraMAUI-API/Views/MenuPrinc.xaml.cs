1238f9235533b3f722eb441233a28891b77cda21
Jorge Isaac Moncayo Loaiza <jorge.moncayo.loaiza@udla.edu.ec>
2024-01-20 23:57:53 -0500

    Ahora se puede actualizar las fotos a las 3 ultimas que se ingreso en la base de datos

--- AuroraMAUI-API/Views/MenuPrinc.xaml.cs
+++ AuroraMAUI-API/Views/MenuPrinc.xaml.cs
@@ -1,9 +1,9 @@
 using AuroraMAUI_API.Models;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace AuroraMAUI_API.Views
 {
@@ -12,13 +12,11 @@ namespace AuroraMAUI_API.Views
         public MenuPrinc()
         {
             InitializeComponent();
-
         }
 
         private void Reservar_Clicked(object sender, EventArgs e)
         {
             Shell.Current.GoToAsync(nameof(ReservaPage));
-
         }
 
         private void VerReservas_Clicked(object sender, EventArgs e)
@@ -29,6 +27,45 @@ namespace AuroraMAUI_API.Views
         private void Regresar_Clicked(object sender, EventArgs e)
         {
             Shell.Current.GoToAsync("..");
+        }
+
+        public async void Actualizar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                string url = "https://localhost:7051/api/Fiestas";
+                var response = await client.GetStringAsync(url);
+
+                var fiestas = JsonConvert.DeserializeObject<List<RootFiestin>>(response);
+
+                var ultimasFiestas = fiestas.OrderByDescending(f => f.idFiesta).Take(3).ToList();
+
+                for (int i = 0; i < ultimasFiestas.Count; i++)
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            Fiesta1.Text = ultimasFiestas[i].nombreFiesta;
+                            Casino.Source = ultimasFiestas[i].imagen;
+                            break;
+                        case 1:
+                            Fiesta2.Text = ultimasFiestas[i].nombreFiesta;
+                            SAVAGE.Source = ultimasFiestas[i].imagen;
+                            break;
+                        case 2:
+                            Fiesta3.Text = ultimasFiestas[i].nombreFiesta;
+                            Zens.Source = ultimasFiestas[i].imagen;
+                            break;
+                    }
+                }
+
+                labelDatos.Text = "Fiestas actualizadas correctamente";
+            }
+            catch (Exception ex)
+            {
+                labelDatos.Text = $"Error al actualizar las fiestas: {ex.Message}";
+            }
         }
     }
 }
